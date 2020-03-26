985dbd2568ae421d2a9ea20f45eab58d9b0213e9
Sébastien Bruere <sebastien.bruere@epitech.eu>
2020-03-26 17:38:18 +0100

    return wip

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,6 +12,10 @@
 //     }
 // })
 
+$(document).ready(() => {
+    fillLink()
+})
+
 
 var buttonFreeAmount = document.getElementById('button-free-amount');
 
@@ -48,3 +52,47 @@ function validateEmail(email) {
     var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(email);
 }
+
+function fillLink() {
+    let p = extractUrlParams();
+    let string = ''
+
+    let code_media = "";
+
+    if (p['reserved_code_media'] && p['reserved_code_media'] !== "undefined") {
+        code_media = p['reserved_code_media'];
+    }
+    else {
+        code_media = 'W20P80ZZL'
+    }
+
+
+    if (p['email'] && p['email'] !== "undefined")
+        string += ("&email=" + p['email']);
+    if (p['wv_email'] && p['wv_email'] !== "undefined")
+        string += ("&email=" + p['wv_email']);
+    if (p['wv_firstname'] && p['wv_firstname'] !== "undefined")
+        string += ("&firstname=" + p['wv_firstname']);
+    if (p['firstname'] && p['firstname'] !== "undefined")
+        string += ("&firstname=" + p['firstname']);
+    if (p['wv_lastname'] && p['wv_lastname'] !== "undefined")
+        string += ("&lastname=" + p['wv_lastname']);
+    if (p['lastname'] && p['lastname'] !== "undefined")
+        string += ("&lastname=" + p['lastname']);
+    string += `&reserved_code_media=${code_media}`;
+
+
+    $('.link-don').each((el) => {
+        $('.link-don').eq(el).attr('href', $('.link-don').eq(el).attr('href') + string);
+    })
+
+}
+
+function extractUrlParams(){
+    var t = document.location.search.substring(1).split('&'); var f = [];
+    for (var i=0; i<t.length; i++){
+        var x = t[ i ].split('=');
+        f[x[0]]=decodeURIComponent(x[1]);
+    }
+    return f;
+};
